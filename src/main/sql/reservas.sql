4ac30ae304f0017a244a348b6f33d4cbbaa554f4
josego16 <jgomlin@g.educaand,es>
2023-11-02 16:06:11 +0100

    creacion de fichero sql reservas

--- src/main/sql/reservas.sql
+++ src/main/sql/reservas.sql
@@ -1,25 +1,30 @@
-CREATE TABLE USUARIO(
-    id PRIMARY KEY,
-    username,
-    password,
-    email,
-    nickname,
-    phone
-);
-CREATE TABLE reserva(
-    id PRIMARY KEY,
-    username,
-    schedule,
-    reserve_date
+CREATE TABLE usuario(
+    id  INT PRIMARY KEY,
+    username VARCHAR(15) UNIQUE NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    email VARCHAR(20) NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    apellidos VARCHAR(20) NOT NULL,
+    telefono INT(9) NOT NULL
 );
 CREATE TABLE instalacion(
-    id PRIMARY KEY,
-    nickname,
-    description
+    id INT PRIMARY KEY,
+    nombre VARCHAR(20) UNIQUE NOT NULL,
+    descripcion VARCHAR(200) NOT NULL
 );
 CREATE TABLE horario(
-    id PRIMARY KEY,
-    start_time,
-    end_time,
-    sport_center
+    id INT PRIMARY KEY,
+    hora_inicio VARCHAR(10) NOT NULL,
+    hora_fin VARCHAR(10) NOT NULL,
+    instalacion INT,
+    FOREIGN KEY (instalacion) REFERENCES instalacion(id)
+
+);
+CREATE TABLE reserva(
+    id INT PRIMARY KEY,
+    usuario INT,
+    horario INT,
+    fecha DATE NOT NULL,
+    FOREIGN KEY (usuario) REFERENCES usuario(id),
+    FOREIGN KEY (horario) REFERENCES horario(id)
 );
